41827a69ed87cd07a83b251e8f50de685741cc4f
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-12 18:37:24 +0900

    어드민 페이지와 쿼리셋 배우기

--- liongram/posts/models.py
+++ liongram/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Post(models.Model):
     image = models.ImageField(verbose_name="이미지", null=True, blank=True)
-    content = models.TimeField(verbose_name='내용')
+    content = models.TextField(verbose_name='내용')
     created_at = models.DateTimeField(verbose_name='작성인', auto_now_add=True)
     view_count = models.IntegerField(verbose_name='조회수', default=0)
     writer = models.ForeignKey(
@@ -16,7 +16,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     content = models.TextField(verbose_name='내용')
-    created_at = models.DateTimeField(verbose_name="작성일")
+    created_at = models.DateTimeField(verbose_name="작성일", auto_now_add=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     writer = models.ForeignKey(
         to=User, on_delete=models.CASCADE, null=True, blank=True)
