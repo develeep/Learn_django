5ed0dbea1efa0ac8e2ca2c39a336f3c263a3dbde
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-17 20:42:27 +0900

    admit 커스텀

--- liongram/posts/admin.py
+++ liongram/posts/admin.py
@@ -1,7 +1,35 @@
+from tabnanny import verbose
 from django.contrib import admin
 
 from posts.models import Post
 from .models import Post, Comment
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 3
+    verbose_name = '댓글'
+    verbose_name_plural = "댓글"
+# 어드민 커스텀
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'content',
+                    'created_at', 'view_count', 'writer',)
+    # list_editable = ('content',)
+    list_filter = ('created_at',)
+    search_fields = ('id', 'writer__username')
+    search_help_text = '게시판 번호, 작성자 검색이 가능합니다.'
+    readonly_fields = ('created_at', 'writer',)
+    inlines = [CommentInline]
+    actions = ['make_published', ]
+
+    def make_published(modeladmin, request, queryset):
+        for item in queryset:
+            item.update(content='운영 규정 위반 삭제 처리.')
+            item.save()
+        # queryset.update(status= 'p')
+
+# admin.site.register(Comment)
